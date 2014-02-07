0a833437dbac337e16a0b52db75b885ee9a84ab4
Austin White <austingym@gmail.com>
2014-02-07 10:24:23 -0800

    Initial commit

---
+++ src/Austinw/Pdfdf/PdfdfServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Austinw\Pdfdf;
+
+use Illuminate\Support\ServiceProvider;
+
+class PdfdfServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('austinw/pdfdf');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
